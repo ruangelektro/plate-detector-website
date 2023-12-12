06f6a3f6e855f21a66fff494d4ead0cb05661a0b
Almer Rashad <ruanggelektro@gmail.com>
2023-12-12 15:41:49 +0700

    first commit

---
+++ public/script.js
@@ -0,0 +1,102 @@
+var myChart=null;
+
+function generateIncrementalArray(arr) {
+    return Array.from({ length: arr.length }, (_, index) => index);
+}
+
+function replaceUndefinedWithZero(arr) {
+    return arr.map(value => (value === undefined ? 0 : value));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  updateValues(); // Call updateValues on page load
+
+  document.getElementById('queryForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    updateValues();
+  });
+});
+
+function updateValues() {
+    if(myChart!=null){
+        myChart.destroy();
+    }
+
+  const param1 = document.getElementById("param1").value;
+  const param2 = document.getElementById("param2").value;
+  const apiUrl = "http://localhost:3000/api/data?bgn=" + param1 + "&end=" + param2;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      const totalDisplay = document.getElementById('totalDisplay');
+      const dishLeftDisplay = document.getElementById('dishLeftDisplay');
+
+      const ctx = document.getElementById('dataChart').getContext('2d');
+
+      const totalLabel = data.totalData;
+      const yValues = data.dishLeftData;
+
+      const incrementalArray = generateIncrementalArray(yValues);
+      const newArray = replaceUndefinedWithZero(totalLabel);
+
+      totalDisplay.textContent = `Total: ${newArray[0]}`;
+      dishLeftDisplay.textContent = `Dish Left: ${yValues[0]}`;
+
+      newArray.reverse();
+      yValues.reverse();
+
+      const xValues = incrementalArray;
+
+      myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: xValues,
+          datasets: [{
+            label: 'Total',
+            data: newArray,
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            fill: false,
+          }, {
+            label: 'Dish Left',
+            data: yValues,
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            fill: false,
+          }],
+        },
+        options: {
+          scales: {
+            x: {
+              type: 'linear',
+              position: 'bottom',
+              title: {
+                display: true,
+                text: 'Timestamp',
+              },
+            },
+            y: {
+              title: {
+                display: true,
+                text: 'Value',
+              },
+            },
+          },
+        },
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      alert('Error fetching data.');
+      const totalDisplay = document.getElementById('totalDisplay');
+      totalDisplay.textContent = 'Error fetching data.';
+      const dishLeftDisplay = document.getElementById('dishLeftDisplay');
+      dishLeftDisplay.textContent = 'Error fetching data.';
+    });
+}
---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/api/data', async (req, res) => {
+    const beginDate = req.query.bgn;
+    const endDate = req.query.end;
+  const externalApiUrl = "https://platform.antares.id:8443/~/antares-cse/antares-id/desproKhalid/desproCam?ty=4&fu=1&drt=2&cra=" + beginDate + "&crb=" + endDate; // Replace with the actual URL
+
+  try {
+    const response = await fetch(externalApiUrl, {
+      method: 'GET',
+      headers: {
+        'X-M2M-Origin': '870f1680796438f0:71e9fc8c307a7e39',
+        'Content-Type': 'application/json;ty=4',
+        'Accept': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+    const dataList = data['m2m:list'];
+
+    // Extracting data for plotting
+    const labels = dataList.map(item => item['m2m:cin'].ct);
+    const totalData = dataList.map(item => JSON.parse(item['m2m:cin'].con).total);
+    const dishLeftData = dataList.map(item => JSON.parse(item['m2m:cin'].con).dishLeft);
+
+    console.log(labels);
+    console.log(totalData);
+    console.log(dishLeftData);
+
+
+    res.json({ labels, totalData, dishLeftData });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
